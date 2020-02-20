9bd05918bb246e15cead1b2c9a6144c9c8e11d51
eliot <eliotvscott@gmail.com>
2020-02-20 11:02:23 -0400

    Initial JWT functions: token issuers and validators.

--- src/config.js
+++ src/config.js
@@ -23,7 +23,19 @@ const Config = {
             }
         },
         apis: ["src/router/handlers/*.js"]
-    }
+    },
+    userPublicKey: process.env.USER_PUBLIC_KEY,
+    userPrivateKey: process.env.USER_PRIVATE_KEY,
+    adminPublicKey: process.env.ADMIN_PUBLIC_KEY,
+    adminPrivateKey: process.env.ADMIN_PRIVATE_KEY,
+    signOptions: {
+        expiresIn: '12h',
+        algorithm: 'RS256'
+    },
+    verifyOptions: {
+        expiresIn: '12h',
+        algorithm: ['RS256']
+    },
 };
 
 module.exports = Config;
---
+++ src/shared/jwt.js
@@ -0,0 +1,90 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+/**
+ * Validates user api JWT.
+ *
+ * If token is invalid (cannot be decoded): return status 500 and error
+ * If token is not in header: return status 401
+ */
+let validateUserToken = (req, res, next) => {
+    let token = req.headers['ha-api-token'];
+
+    if (token) {
+        jwt.verify(token, config.userPublicKey, config.verifyOptions, (err) => {
+            if (err)
+                res.status(500).send(err);
+
+            next();
+        });
+    }
+
+    return res.status(401).send();
+};
+
+/**
+ * Validates admin api JWT.
+ *
+ * If token is invalid (cannot be decoded): return status 500 and error
+ * If token is not in header: return status 401
+ */
+let validateAdminToken = (req, res, next) => {
+    let token = req.headers['ha-admin-token'];
+
+    if (token) {
+        jwt.verify(token, config.adminPublicKey, config.verifyOptions, (err, decoded) => {
+            if (err)
+                res.status(500).send(err);
+
+            next();
+        });
+    }
+
+    return res.status(401).send();
+};
+
+/**
+ * Validates user API request by decoding JWT and comparing decoded UUID against query param UUID.
+ * If token is invalid (cannot be decoded): return status 500 and error
+ *
+ * If token is invalid (cannot be decoded): return status 500 and error
+ * If token is not in header: return status 401
+ */
+let validateUserRequest = (req, res, next) => {
+    let token = req.headers['ha-admin-token'];
+    let uid = req.query.uid;
+
+    if (token) {
+        jwt.verify(token, config.adminPublicKey, config.verifyOptions, (err, decoded) => {
+            if (err)
+                res.status(500).send(err);
+            if (decoded.uid !== uid)
+                res.status(403).send();
+            next();
+        });
+    }
+
+    return res.status(401).send();
+};
+
+/**
+ * Issues a user token with an encoded UID.
+ */
+let issueUserToken = (uid) => {
+    return jwt.sign({uid: uid}, config.userPrivateKey, config.signOptions);
+};
+
+/**
+ * Issues an admin token with an encoded UID.
+ */
+let issueAdminToken = (uid) => {
+    return jwt.sign({uid: uid}, config.adminPrivateKey, config.signOptions);
+};
+
+module.exports = {
+    validateUserToken,
+    validateAdminToken,
+    validateUserRequest,
+    issueUserToken,
+    issueAdminToken
+};
