0564f5c7aa4037f4345e0a20acf12a9eff3cb4f2
eliot <eliotvscott@gmail.com>
2020-02-23 12:28:32 -0400

    Small fix

--- src/shared/jwt.js
+++ src/shared/jwt.js
@@ -51,7 +51,7 @@ let validateAdminToken = (req, res, next) => {
  * If token is not in header: return status 401
  */
 let validateUserRequest = (req, res, next) => {
-    let token = req.headers['ha-admin-token'];
+    let token = req.headers['ha-api-token'];
     let uid = req.query.uid;
 
     if (token) {
